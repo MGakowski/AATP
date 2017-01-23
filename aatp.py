6d245ed2d2c85d666582b7f5ace0d736b409b9ce
Cyph3r - Matt Gakowski <mgakowski@gmail.com>
2017-01-23 19:59:34 +1000

    Update aatp.py

--- aatp.py
+++ aatp.py
@@ -1,9 +1,11 @@
+import os
+
 createfile = int(raw_input("Create separate file=1, Overwrite=2 "))
-pyver = int(raw_input("Pyhton version 2 or 3?"))
+pyver = int(raw_input("Pyhton version 2 or 3? "))
+filen = str()
 
-
+# New File
 if createfile == 1:
-    # Gets filenames
     filename = str(raw_input("File name of target ascii art? "))
     newfile = str(raw_input("New file name? "))
     write_file = open(newfile + '.txt', "w")
@@ -24,10 +26,13 @@ if createfile == 1:
         currentl += 1
     write_file.close()
 
+# Overwrite
 if createfile == 2:
-    # Gets filenames
     filename = str(raw_input("File name of target ascii art? "))
-    read_file = open(filename + '.txt', "w+")
+    filen = filename
+    newfile = str(filename + "_temp")
+    write_file = open(newfile + '.txt', "w")
+    read_file = open(filename + '.txt', "r")
 
     currentl = 0
 
@@ -35,11 +40,14 @@ if createfile == 2:
         linecount = sum(1 for _ in f)
 
     for x in range(0, linecount):
-        #read_it = read_file.read().splitlines()[currentl]
+        read_file = open(filename + '.txt', "r")
+        read_it = read_file.read().splitlines()[currentl]
         if pyver == 2:
-            read_file.write('print "' + read_file.read().splitlines()[currentl] + '"')
-            print "writing"
+            write_file.write('print "' + read_it + '"\n')
         else:
-            read_file.write('print("' + read_file.read().splitlines()[currentl] + '")')
+            write_file.write('print("' + read_it + '")\n')
         currentl += 1
     read_file.close()
+    write_file.close()
+    os.remove(filename + ".txt")
+    os.rename(filen + "_temp.txt", filen + ".txt")
